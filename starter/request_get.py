9da12f6b55963ef816d473724ef5e3dcbb9e469c
nhattran134 <nhattran134@gmail.com>
2023-06-21 13:06:07 +0700

    [skip ci] update get check

--- starter/request_get.py
+++ starter/request_get.py
@@ -1,7 +1,8 @@
 import json
 import requests
 
-response = requests.get('http://udacity-nd0821-prod.eba-misicvr9.us-east-1.elasticbeanstalk.com/', data=json.dumps(data))
+
+response = requests.get('http://udacity-nd0821-prod.eba-misicvr9.us-east-1.elasticbeanstalk.com/')
 
 print(response.status_code)
 print(response.json())
